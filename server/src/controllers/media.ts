5ee7be5b93f6e13eecfe10b65945a0e45c971f1d
marcSitze <marcsitze01@gmail.com>
2021-10-18 16:53:11 +0100

    media controller

--- server/src/controllers/media.ts
+++ server/src/controllers/media.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
+import { create } from '../common/upload';
+export const createMedia = async (req: Request, res: Response) => {
+  const result =  create(req, res);
+  res.send(result);
+}
+export const getMedia = async (req: Request, res: Response) => {
 
-export const createMedia = async (req: Request, res: Response) => {}
-export const getMedia = async (req: Request, res: Response) => {}
+}
 export const getMedias = async (req: Request, res: Response) => {}
 export const updateMedia = async (req: Request, res: Response) => {}
