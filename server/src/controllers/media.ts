908395cc2b0200d68ce2fa8508572d63930d7101
marcSitze <marcsitze01@gmail.com>
2021-11-03 15:59:21 +0100

    create media

--- server/src/controllers/media.ts
+++ server/src/controllers/media.ts
@@ -1,8 +1,33 @@
 import { Request, Response } from 'express';
-import { create } from '../common/upload';
-export const createMedia = async (req: Request, res: Response) => {
-  const result =  create(req, res);
-  res.send(result);
+import fs from 'fs';
+import { ErrorHandler, SuccessHandler } from '../common/response.handler';
+import constants from '../common/constants';
+import { createMedia as saveMedia } from '../common/upload';
+
+const { httpStatus } = constants;
+
+export const createMedia = async (req: any, res: any) => {
+
+  console.log('req.media: ', req.file);
+  if(req.file && req.file.size > 1024 * 1024) {
+    fs.unlink(req.file.path, function(err) {
+      if(err) throw err;
+    });
+    return ErrorHandler(res, httpStatus.BAD_REQUEST, "please upload an image or gif <= 1mo");
+  }
+  try {
+    const result = await saveMedia({
+      name: 'abcd',
+      path: req.file.path,
+      mimetype: req.file.mimetype
+    }, '61828045c2470b1ac499542a');
+    console.log('result: ', result);
+    SuccessHandler(res, httpStatus.CREATED, result);
+  } catch(err) {
+    console.error(err);
+  }
+
+
 }
 export const getMedia = async (req: Request, res: Response) => {
 
