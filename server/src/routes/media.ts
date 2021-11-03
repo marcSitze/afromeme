e1aebd6d0e1842562e3ad3b64ef62941663d95e1
marcSitze <marcsitze01@gmail.com>
2021-11-03 16:00:26 +0100

    upload middleware

--- server/src/routes/media.ts
+++ server/src/routes/media.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
 import { auth } from '../middlewares/auth/auth';
 import { createMedia, getMedia, getMedias, updateMedia } from '../controllers/media';
+import { upload } from '../common/upload';
 
 
 
 const router = express.Router();
 
-router.post('/', createMedia);
+router.post('/', upload.single('photo'), createMedia);
 router.get('/:id', getMedia);
 router.get('/', getMedias);
 router.put('/:id', updateMedia);
