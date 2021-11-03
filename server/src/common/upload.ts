ed1dafc9db40e8913274a70b42e528e44b02d5fa
marcSitze <marcsitze01@gmail.com>
2021-11-03 15:58:49 +0100

    file filter

--- server/src/common/upload.ts
+++ server/src/common/upload.ts
@@ -1,46 +1,97 @@
-// import * as multer from 'multer';
-// import * as path from 'path';
-// const UPLOAD_PATH: string = 'uploads';
+import multer from 'multer';
+import * as path from 'path';
+import fs from 'fs';
+import MediaService from '../services/media.service';
 
-// const upload = multer({ dest: `${UPLOAD_PATH}/`, fileFilter: imageFilter }); // apply filter
+const UPLOAD_PATH: string = 'uploads';
+const mediaService = new MediaService();
 
-// const imageFilter = function (req, file, cb) {
-//   // accept image only
-//   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-//       return cb(new Error('Only image files are allowed!'), false);
-//   }
-//   cb(null, true);
-// };
-import { Request, Response } from 'express';
-import formidable, { Files } from 'formidable';
-import IncomingForm from 'formidable/Formidable';
+const imageFilter = function (req: any, file: any, cb: any) {
+  console.log('file: ', req.file);
+  req.media = req.file;
+  // accept image only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
 
-export const create = (req: Request, res: Response) => {
-  let form: IncomingForm = new formidable.IncomingForm({
-    uploadDir: __dirname + '../../../tmp',
-    keepExtensions: true
-  });
-  // form.keepExtensions = true;
-  form.parse(req, (err, fields, files: Files) => {
-    if(err) {
-      return res.status(400).json({"error": "Image could not be uploaded"});
+export const upload = multer({ dest: `${UPLOAD_PATH}/`, fileFilter: imageFilter }); // apply filter
+
+export const createMedia = async (file: any, userId: string) => {
+  const media = {
+    name: file.name,
+    author: userId,
+    photo: {
+      data: fs.readFileSync(file.path),
+      contentType: file.mimetype
     }
-    // 1kb = 1000
-    // 1mb = 1000000
-
-    if(files.photo) {
-      console.log('Files photo: ', files.photo);
-      // if(files.photo.size > 1000000) {
-      //   return resizeBy.status(400).json({"error": "image should be less than 1mb in size"})
-      // }
-
-      // save image in db
-      // product.photo.data = fs.readFileSync(files.photo.path);
-      // product.photo.contentType = fs.readFileSync(files.photo.type);
-      return files.photo;
-    }
-  });
-
-
-
+  };
+  try {
+    const newMedia = await mediaService.createMedia(media);
+    return newMedia;
+  } catch (err) {
+    console.error(err);
+  }
 }
+
+
+// import { Request, Response } from 'express';
+// import formidable, { Files } from 'formidable';
+// import fs from 'fs';
+// import IncomingForm from 'formidable/Formidable';
+// import MediaService from '../services/media.service';
+
+// const mediaService = new MediaService();
+// export const create = (req: Request, res: Response, userId: string): any => {
+//   let form: IncomingForm = new formidable.IncomingForm({
+//     uploadDir: __dirname + '../../../tmp',
+//     keepExtensions: true,
+//     maxFileSize: 1 * 1024 * 1024,
+//   });
+//   const errors: any = [];
+//   // form.keepExtensions = true;
+//   form.parse(req, async (err, fields, files: any) => {
+//     if(err) {
+//       errors.push({msg: "Image could not be uploaded..."});
+//       // return res.status(400).json({"error": "Image could not be uploaded"});
+//       return errors;
+//     }
+//     // 1kb = 1000
+//     // 1mb = 1000000
+
+//     if(files.photo) {
+//       console.log('Files photo: ', files.photo);
+//       // if(files.photo.size > 1000000) {
+//       //   errors.push({msg: "image should be less than 1mb in size"});
+//       //   // return res.status(400).json({"error": "image should be less than 1mb in size"})
+//       // console.log('errors: ', errors);
+//       //   return errors;
+//       // }
+
+//       // save image in db
+//       // product.photo.data = fs.readFileSync(files.photo.path);
+//       // product.photo.contentType = fs.readFileSync(files.photo.type);
+//       const media = {
+//         name: files.photo.name.split('.')[0],
+//         author: userId,
+//         photo: {
+//           data: fs.readFileSync(files.photo.path),
+//           contentType: files.photo.type,
+//         },
+//         description: '',
+
+//       }
+//       try {
+//         const newMedia = await mediaService.createMedia(media);
+//         console.log('new media: ', newMedia);
+//         return newMedia;
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     }
+//   });
+
+
+
+// }
