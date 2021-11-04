e8f6c99dd23c7d443a62b598286e8a9cdc360121
marcSitze <marcsitze01@gmail.com>
2021-11-04 15:17:32 +0100

    post service interface

--- server/src/interfaces/posts/post.service.interface.ts
+++ server/src/interfaces/posts/post.service.interface.ts
@@ -2,7 +2,7 @@ import IPost from './post.interface';
 import { CreatePostDTO } from '../../dto/post.dto';
 
 export interface IPostsService{
-    createPost(user: CreatePostDTO): Promise<IPost | void>;
+    createPost(post: CreatePostDTO): Promise<IPost | void>;
     getPosts(): Promise<IPost[] | void>;
     findOne(query: any): Promise<IPost | void | any>;
     getPostById(id: string): Promise<IPost | void | any>;
