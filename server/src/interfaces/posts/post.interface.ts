18ac9eee16f999e4d495bef59cbfcc6621cd4a3e
marcSitze <marcsitze01@gmail.com>
2021-11-04 15:17:15 +0100

    post interface

--- server/src/interfaces/posts/post.interface.ts
+++ server/src/interfaces/posts/post.interface.ts
@@ -1,9 +1,9 @@
 export default interface IPost {
-    _id?: string;
+    _id: string;
     author: string;
     description: string;
     media: string;
-    comments?: string[];
+    comments: string[];
     likes?: string[];
     createdAt?: Date;
     updatedAt?: Date;
