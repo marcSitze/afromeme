4b5b026f964980549493a357bb367d6864574a92
marcSitze <marcsitze01@gmail.com>
2021-11-04 15:16:47 +0100

    post model interface

--- server/src/interfaces/models/AccountModel.ts
+++ server/src/interfaces/models/AccountModel.ts
@@ -1,10 +1,11 @@
 import { Document } from 'mongoose';
 import { IBio } from '../account/bio.interface';
-
+import IPost from '../posts/post.interface';
 export interface AccountDocument extends Document{
     _id: string;
     user: string;
     location: string;
+    posts: string[];
     followers: string[];
     bio: IBio;
     createdAt?: Date;
