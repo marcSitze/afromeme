5db4e9b827cdd9c0d3b55d92ef09734f8ed3cb66
marcSitze <marcsitze01@gmail.com>
2021-11-04 15:19:02 +0100

    nedia routes

--- server/src/routes/media.ts
+++ server/src/routes/media.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { auth } from '../middlewares/auth/auth';
-import { createMedia, getMedia, getMedias, updateMedia } from '../controllers/media';
+import { createMedia, getMedia, getMedias, updateMedia, photo } from '../controllers/media';
 import { upload } from '../common/upload';
 
 
@@ -8,7 +8,7 @@ import { upload } from '../common/upload';
 const router = express.Router();
 
 router.post('/', upload.single('photo'), createMedia);
-router.get('/:id', getMedia);
+router.get('/:id', getMedia, photo);
 router.get('/', getMedias);
 router.put('/:id', updateMedia);
 
