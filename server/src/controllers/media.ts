b63ae9c3051f015d199fb7fbcc65b193fbf16711
marcSitze <marcsitze01@gmail.com>
2021-11-04 15:15:15 +0100

    media controller

--- server/src/controllers/media.ts
+++ server/src/controllers/media.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import fs from 'fs';
 import { ErrorHandler, SuccessHandler } from '../common/response.handler';
 import constants from '../common/constants';
 import { createMedia as saveMedia } from '../common/upload';
+import MediaService from '../services/media.service';
 
 const { httpStatus } = constants;
+const mediaService = new MediaService();
 
 export const createMedia = async (req: any, res: any) => {
 
@@ -20,7 +22,7 @@ export const createMedia = async (req: any, res: any) => {
       name: 'abcd',
       path: req.file.path,
       mimetype: req.file.mimetype
-    }, '61828045c2470b1ac499542a');
+    }, '61828045c2470b1ac499542b');
     console.log('result: ', result);
     SuccessHandler(res, httpStatus.CREATED, result);
   } catch(err) {
@@ -29,8 +31,26 @@ export const createMedia = async (req: any, res: any) => {
 
 
 }
-export const getMedia = async (req: Request, res: Response) => {
-
+export const getMedia = async (req: any, res: Response) => {
+  const media = await mediaService.getMediaById(req.params.id);
+  if(media) {
+  req.media = media;
+  console.log('media', media);
+  res.set('Content-Type', media.photo.contentType)
+  res.send(media.photo.data);
+  }
 }
-export const getMedias = async (req: Request, res: Response) => {}
+
+export const photo = (req: any, res: any, next: NextFunction) => {
+  console.log('req.photo: ', req.photo);
+  if(req.photo.photo.data) {
+    res.set('Content-Type', req.photo.photo.contentType);
+    res.send(req.photo.photo.data);
+  }
+  next();
+}
+export const getMedias = async (req: Request, res: Response) => {
+  const medias = await mediaService.getMedias();
+  res.send(medias);
+}
 export const updateMedia = async (req: Request, res: Response) => {}
