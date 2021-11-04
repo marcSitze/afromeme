71a93baae1ea09c280c8b1aeb9fe52cfd005cb0d
marcSitze <marcsitze01@gmail.com>
2021-11-04 15:16:27 +0100

    account interface

--- server/src/interfaces/account/account.interface.ts
+++ server/src/interfaces/account/account.interface.ts
@@ -1,9 +1,11 @@
 import  { IBio } from './bio.interface';
+import IPost from '../posts/post.interface';
 
 export interface IAccount {
     _id?: string;
     user: string;
     location: string;
+    posts: string[]
     followers: string[];
     bio: IBio;
     createdAt?: Date;
