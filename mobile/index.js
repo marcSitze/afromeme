224fb56905a6a6c37cd59a0517b79918be68d673
marcSitze <marcsitze01@gmail.com>
2022-03-26 10:32:48 +0100

    import native base provider

--- mobile/index.js
+++ mobile/index.js
@@ -3,7 +3,19 @@
  */
 
 import {AppRegistry} from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import React from 'react';
 import App from './App';
 import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
+import store from './src/redux/setup/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const MyApp = () => (
+  <Provider store={store}>
+    <NativeBaseProvider>
+      <App />
+    </NativeBaseProvider>
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => MyApp);
