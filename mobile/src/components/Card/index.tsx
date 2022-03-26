9d07e73c59f41866a4d090a21c1962381a1f12c0
marcSitze <marcsitze01@gmail.com>
2022-03-26 10:33:45 +0100

    use native base components

--- mobile/src/components/Card/index.tsx
+++ mobile/src/components/Card/index.tsx
@@ -1,22 +1,33 @@
 import React from "react";
-import { View, Text } from 'react-native';
+// import { View, Text, TouchableOpacity } from 'react-native';
+import { Center, Container, Heading, Text } from 'native-base';
 import { connect } from "react-redux";
 import styles from './styles';
 import { IPost } from '../../types/posts';
-import { PropsState } from '../../types';
+import { PropsState, HomeTabParamList } from '../../types';
 
 type Props = {
   text?: string,
   style?: any,
   posts?: Array<IPost>,
+  navigation: HomeTabParamList
 }
-const Card = ({ text = 'Hello Text', style, posts }: Props) => {
+const Card = ({ text = 'Hello Text', style, posts, navigation }: Props) => {
   console.log('posts: ', posts);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{text}</Text>
-    </View>
+    <Center>
+    <Container>
+      <Heading>
+          A component library for the
+          <Text color="emerald.500"> React Ecosystem</Text>
+        </Heading>
+        <Text mt="3" fontWeight="medium">
+          NativeBase is a simple, modular and accessible component library that
+          gives you building blocks to build you React applications.
+        </Text>
+    </Container>
+    </Center>
   )
 }
 
