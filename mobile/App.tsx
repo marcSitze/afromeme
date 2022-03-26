2d3da6fc0bfc902cda32f3b4c7ff3cfae7557bc4
marcSitze <marcsitze01@gmail.com>
2022-03-26 10:32:26 +0100

    clean app file

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,52 +7,14 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  LogBox
-} from 'react-native';
-import { Provider } from 'react-redux';
-import store from './src/redux/setup/store';
-import Card from './src/components/Card'
+import {LogBox} from 'react-native';
 import Routes from './src/routes/Routes';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  LogBox.ignoreLogs(["[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!"])
-  return (
-    <Provider store={store}>
-      <SafeAreaView>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <Routes />
-      </SafeAreaView>
-    </Provider>
-  );
+  LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+  ]);
+  return <Routes />;
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
