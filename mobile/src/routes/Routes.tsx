90117ad30e1d7a453341b98d9b44038b02e89c4b
marcSitze <marcsitze01@gmail.com>
2022-03-30 23:00:32 +0100

    add register screen

--- mobile/src/routes/Routes.tsx
+++ mobile/src/routes/Routes.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {RootStackParamList} from '../types';
 import * as SCREENS from '../constants/screens';
 import Card from '../components/Card';
-import {RootStackParamList} from '../types';
+import Login from '../screens/Auth/Login';
+import Register from '../screens/Auth/Register';
 
-const RootStack = createStackNavigator<RootStackParamList>();
+const stack = createStackNavigator<RootStackParamList>();
+
+const screenOptions ={
+  headerShown: false,
+};
+
 export default function Routes() {
   return (
     <NavigationContainer>
-      <RootStack.Navigator initialRouteName={SCREENS.HOME}>
-        <RootStack.Screen name={SCREENS.HOME} component={Card} />
-      </RootStack.Navigator>
+      <stack.Navigator screenOptions={screenOptions} initialRouteName={SCREENS.REGISTER}>
+        <stack.Screen name={SCREENS.HOME} component={Card} />
+        <stack.Screen name={SCREENS.LOGIN} component={Login} />
+        <stack.Screen name={SCREENS.REGISTER} component={Register} />
+      </stack.Navigator>
     </NavigationContainer>
   );
 }
