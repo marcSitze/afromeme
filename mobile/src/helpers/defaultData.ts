6def745c3e421311c503f820f3f9216e77d103e7
marcSitze <marcsitze01@gmail.com>
2022-04-03 12:53:38 +0100

    default data

---
+++ mobile/src/helpers/defaultData.ts
@@ -0,0 +1,28 @@
+import img from '../assets/images/image.png';
+
+export const posts = [
+  {
+    author: {
+      name: 'Jake Paul'
+    },
+    media: img,
+  },
+  {
+    author: {
+      name: 'Mario Super'
+    },
+    media: img,
+  },
+  {
+    author: {
+      name: 'Luigui'
+    },
+    media: img,
+  },
+  {
+    author: {
+      name: 'Jane Doe'
+    },
+    media: img,
+  }
+]
