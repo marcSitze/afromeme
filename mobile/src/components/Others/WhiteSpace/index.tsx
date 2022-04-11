b1768a74c774f2338f11388b9856300d6a9cbabb
marcSitze <marcsitze01@gmail.com>
2022-04-11 10:07:47 +0100

    other components

---
+++ mobile/src/components/Others/WhiteSpace/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box } from 'native-base'
+
+const WhiteSpace = ({ height}: { height: number }) => {
+  return (
+    <Box h={height}></Box>
+  )
+}
+
+export default WhiteSpace
