1852afd577bd4264ade3e7bd773f27673651216a
marcSitze <marcsitze01@gmail.com>
2022-04-11 10:08:44 +0100

    Upload screen

---
+++ mobile/src/screens/Upload/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Modal, Animated } from 'react-native';
+import { Box, Text } from 'native-base';
+
+/**
+ * 
+ * @add react native image picker for image selection
+ */
+
+const Upload = () => {
+  return (
+    // <Modal animationType='slide' transparent={true}>
+    <Box>
+      <Text>Upload an image of a gif</Text>
+    </Box>
+    // </Modal>
+  );
+}
+
+export default Upload
