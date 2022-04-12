44178ff9a138b4bd90a59e8016853592df991ab8
marcSitze <marcsitze01@gmail.com>
2022-04-12 10:45:48 +0100

    add auth reducer

--- mobile/src/redux/setup/reducer.ts
+++ mobile/src/redux/setup/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import postsReducer from "../posts/reducer";
+import authReducer from "../auth/reducer";
 
 const reducerCombination = combineReducers({
     postsReducer,
+    authReducer,
 });
 
 export default reducerCombination;
