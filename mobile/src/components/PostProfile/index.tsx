005e3ceb21f5ee44898b15c5df0148fb8def2346
marcSitze <marcsitze01@gmail.com>
2022-05-26 21:21:44 +0100

    show post from api

--- mobile/src/components/PostProfile/index.tsx
+++ mobile/src/components/PostProfile/index.tsx
@@ -4,11 +4,12 @@ import { Image } from 'react-native';
 import img from '../../assets/images/meme1.jpg';
 import { width, height } from '../../constants/layout';
 import { PropTypes } from './PropTypes';
+import config from '../../config';
 
 const PostProfile = ({ media }: PropTypes) => {
   return (
-    <Box width={(width / 3) - 10} height={height / 6}>
-      <Image style={{ width: '100%', height: '100%'}} source={img} />
+    <Box width={(width / 3) - 10} height={height / 6} borderWidth={2} borderColor={'white'}>
+      <Image style={{ width: '100%', height: '100%'}} source={{ uri: config.API + '/api/media/' + media}} />
     </Box>
   )
 }
