e8d9fa4f21b9feea7b7dd9cbd498b5bfb68715b9
marcSitze <marcsitze01@gmail.com>
2022-05-26 21:22:33 +0100

    view profile screen

--- mobile/src/constants/screens.ts
+++ mobile/src/constants/screens.ts
@@ -5,3 +5,4 @@ export const BOTTOM_NAVIGATION = 'BOTTOM_NAVIGATION';
 export const WAITING_SCREEN = 'WAITING_SCREEN';
 export const PROFILE = 'PROFILE';
 export const UPLOAD = 'UPLOAD';
+export const VIEW_PROFILE = 'VIEW_PROFILE';
