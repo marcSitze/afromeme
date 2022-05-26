b00e4d80fb91313e12d817275ce6befa85054b2a
marcSitze <marcsitze01@gmail.com>
2022-05-26 21:31:00 +0100

    postscreen

---
+++ mobile/src/screens/PostsScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from 'native-base';
+
+import styles from './styles';
+
+const PostsScreen = () => {
+  return (
+    <Box></Box>
+  )
+}
+
+export default PostsScreen;
