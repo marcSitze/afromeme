304fbfd9dbb2e1f8b48696a174c54adccdd38c9f
marcSitze <marcsitze01@gmail.com>
2022-07-12 21:58:07 +0100

    actions posts

--- mobile/src/redux/posts/actions.ts
+++ mobile/src/redux/posts/actions.ts
@@ -1,9 +1,10 @@
-import { GET_POSTS_REQUEST, CREATE_POST_REQUEST } from './types';
+import {GET_POSTS_REQUEST, CREATE_POST_REQUEST} from './types';
 
 export const getPosts = () => ({
-    type: GET_POSTS_REQUEST
-})
+  type: GET_POSTS_REQUEST,
+});
 
 export const createPost = (payload: any) => ({
-    type: CREATE_POST_REQUEST, payload
-})
+  type: CREATE_POST_REQUEST,
+  payload,
+});
