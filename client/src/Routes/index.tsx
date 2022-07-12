bec4776f30715538b18209003a35e363e593948c
marcSitze <marcsitze01@gmail.com>
2022-07-12 21:54:49 +0100

    Routes

---
+++ client/src/Routes/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as Switch,
+} from "react-router-dom";
+
+import Register from '../pages/Auth/Register';
+import Home from '../components/Home';
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" element={Home} />
+        {/* <Route path="/register" element={Register} /> */}
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes;
