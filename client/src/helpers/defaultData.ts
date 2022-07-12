f4ead9dd1583930273980bc1f3ac8ae09c43f2f3
marcSitze <marcsitze01@gmail.com>
2022-07-12 21:49:42 +0100

    default data

---
+++ client/src/helpers/defaultData.ts
@@ -0,0 +1,81 @@
+import img from '../assets/images/image.png';
+import img1 from '../assets/images/meme1.jpg';
+
+export const posts = [
+  {
+    author: {
+      name: 'Jake Paul'
+    },
+    media: img1,
+  },
+  {
+    author: {
+      name: 'Mario Super'
+    },
+    media: img1,
+  },
+  {
+    author: {
+      name: 'Luigui'
+    },
+    media: img1,
+  },
+  {
+    author: {
+      name: 'Jane Doe'
+    },
+    media: img1,
+  }
+]
+
+export const profilePosts = [
+  {
+    id: 1,
+    author: {
+      name: 'Jake Paul'
+    },
+    media: img1,
+  },
+  {
+    id: 2,
+    author: {
+      name: 'Mario Super'
+    },
+    media: img1,
+  },
+  {
+    id: 3,
+    author: {
+      name: 'Luigui'
+    },
+    media: img1,
+  },
+  {
+    id: 4,
+    author: {
+      name: 'Jane Doe'
+    },
+    media: img1,
+  },
+  {
+    id: 5,
+    author: {
+      name: 'Jane Doe'
+    },
+    media: img1,
+  },
+  {
+    id: 6,
+    author: {
+      name: 'Jane Doe'
+    },
+    media: img1,
+  },
+  {
+    id: 7,
+    author: {
+      name: 'Jane Doe'
+    },
+    media: img1,
+  },
+]
