21b7687837f39332f7b675d6296c8ed098dc4492
marcSitze <marcsitze01@gmail.com>
2022-07-12 21:56:17 +0100

    auth types

---
+++ client/src/types/auth/index.ts
@@ -0,0 +1,32 @@
+export interface IUser {
+  _id: string,
+  firstName?: string,
+  lastName: string
+}
+
+export interface IAccount {
+  user: string,
+}
+
+export interface IAuthState {
+  user: IUser,
+  account: IAccount,
+  logging: Boolean,
+  logging_error: string | void,
+  error: string | void,
+  registering: Boolean,
+  registering_error: string | void,
+  token: string,
+}
+
+export type LoginDto = {
+  email: string;
+  password: string;
+};
+
+export type RegisterDto = {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
