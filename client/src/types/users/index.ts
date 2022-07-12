b4c720070d6ef90f435e3f43c774e7527426595e
marcSitze <marcsitze01@gmail.com>
2022-07-12 21:56:40 +0100

    users types

---
+++ client/src/types/users/index.ts
@@ -0,0 +1,19 @@
+export interface IUser {
+  _id: string,
+  firstName?: string,
+  lastName: string,
+  username: string,
+}
+
+export interface IAccount {
+  _id: string,
+  user: any, // todo add user props
+  posts: Array<any>, // tobe defined
+  followers: Array<any> // tobe defined
+}
+
+export interface IUserState {
+  user: IUser,
+  account: IAccount,
+  error: string | void
+}
