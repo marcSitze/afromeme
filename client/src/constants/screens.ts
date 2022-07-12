92e2ca8139ea083923b0b7cb7d857fe29acb81fe
marcSitze <marcsitze01@gmail.com>
2022-07-12 21:49:19 +0100

    screens names

---
+++ client/src/constants/screens.ts
@@ -0,0 +1,8 @@
+export const HOME = 'HOME';
+export const LOGIN = 'LOGIN';
+export const REGISTER = 'REGISTER';
+export const BOTTOM_NAVIGATION = 'BOTTOM_NAVIGATION';
+export const WAITING_SCREEN = 'WAITING_SCREEN';
+export const PROFILE = 'PROFILE';
+export const UPLOAD = 'UPLOAD';
+export const VIEW_PROFILE = 'VIEW_PROFILE';
