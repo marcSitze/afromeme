b394892214b75c19e8ad24ad856adb4959cf3e9d
marcSitze <marcsitze01@gmail.com>
2022-07-13 16:36:58 +0100

    ejs for reset password

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('combined'));
 
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 // static folders
 // app.use(express.static(path.join(__dirname, "/public")));
 // app.use(express.static(path.join(__dirname, "/ups")));
