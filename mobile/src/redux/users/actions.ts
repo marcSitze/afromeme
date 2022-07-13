39be1ce9948e8c837726f02b9bb889b5b46068fa
marcSitze <marcsitze01@gmail.com>
2022-07-13 16:32:52 +0100

    get local user account

--- mobile/src/redux/users/actions.ts
+++ mobile/src/redux/users/actions.ts
@@ -2,5 +2,10 @@ import * as types from './types';
 
 export const getUserAccount = (userId: string) => ({
   type: types.GET_USER_ACCOUNT_REQUEST,
-  userId
-})
+  userId,
+});
+
+export const getLocalUserAccount = (userId: string) => ({
+  type: types.GET_LOCAL_USER_ACCOUNT_REQUEST,
+  userId,
+});
