cb150aa4aa1af530b1f67d87bb8565c44cf704fd
marcSitze <marcsitze01@gmail.com>
2022-07-13 16:38:56 +0100

    token model

---
+++ server/src/models/Token.ts
@@ -0,0 +1,23 @@
+
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const tokenSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+    expires: 600000,
+  },
+});
+
+export default mongoose.model("Token", tokenSchema);
