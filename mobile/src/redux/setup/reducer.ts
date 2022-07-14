6c8aebc32265191b294d4b31ae547a4c16e88036
marcSitze <marcsitze01@gmail.com>
2022-07-14 23:19:51 +0100

    add comments reducer

--- mobile/src/redux/setup/reducer.ts
+++ mobile/src/redux/setup/reducer.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import postsReducer from "../posts/reducer";
 import authReducer from "../auth/reducer";
-import usersReducer from '../users/reducer';
+import usersReducer from '../users/reducer'
+import commentsReducer from "../comments/reducer";
 
 const reducerCombination = combineReducers({
     postsReducer,
     authReducer,
     usersReducer,
+    commentsReducer,
 });
 
 export default reducerCombination;
