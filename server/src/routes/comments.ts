abec41a5870a1525017ad45f51889bb76040c8cd
marcSitze <marcsitze01@gmail.com>
2022-07-14 23:26:46 +0100

    auth middleware

--- server/src/routes/comments.ts
+++ server/src/routes/comments.ts
@@ -1,15 +1,21 @@
-import * as express from 'express';
-import { createComment, getComment, getComments, updateComment } from '../controllers/comments';
-import { auth } from '../middlewares/auth/auth';
+import * as express from "express";
+import {
+  createComment,
+  getComment,
+  getComments,
+  updateComment,
+  getCommentsByQuery,
+} from "../controllers/comments";
+import { auth } from "../middlewares/auth/auth";
 const router = express.Router();
 
-
 // Use the jsonewebtoken middleware
-// router.use(auth);
+router.use(auth);
 
-router.post('/', createComment);
-router.get('/:id', getComment);
-router.get('/', getComments);
-router.put('/:id', auth, updateComment);
+router.post("/", createComment);
+router.get("/:id", getComment);
+router.get("/", getComments);
+router.get("/query/q?", getCommentsByQuery);
+router.put("/:id", updateComment);
 
 export default router;
