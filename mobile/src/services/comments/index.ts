1b367e81571eddc6f0476d642317c19b81adf9dc
marcSitze <marcsitze01@gmail.com>
2022-07-14 23:20:57 +0100

    comments service

---
+++ mobile/src/services/comments/index.ts
@@ -0,0 +1,46 @@
+import config from '../../config';
+import {CommentDto} from '../../types/comments';
+var myHeaders = new Headers();
+myHeaders.append('Content-Type', 'application/json');
+
+export const createComment = async (token: string, payload: CommentDto) => {
+  var raw = JSON.stringify({
+    author: payload.author,
+    message: payload.message,
+    post: payload.post,
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    body: raw,
+    headers: myHeaders,
+  };
+
+  try {
+    myHeaders.append('Authorization', 'Bearer ' + token);
+    const request = await fetch(config.API + '/api/comments/', requestOptions);
+    const result = await request.json();
+    return result;
+  } catch (err) {
+    console.error('CreateCommentService: ', err);
+  }
+};
+
+export const getComments = async (token: string, post: string) => {
+  try {
+    console.log("post: ", post);
+    myHeaders.append('Authorization', 'Bearer ' + token);
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+    };
+    const request = await fetch(
+      config.API + '/api/comments/query/q?post=' + post,
+      requestOptions,
+    );
+    const result = await request.json();
+    return result;
+  } catch (err) {
+    console.error('GetCommentsErrSer: ', err);
+  }
+};
