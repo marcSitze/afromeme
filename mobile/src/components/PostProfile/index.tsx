51237b2bacd6c9874c7a5bebc71af860d153e4f6
marcSitze <marcsitze01@gmail.com>
2022-07-15 19:02:52 +0100

    update props

--- mobile/src/components/PostProfile/index.tsx
+++ mobile/src/components/PostProfile/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Box } from 'native-base';
-import { Image } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import img from '../../assets/images/meme1.jpg';
 import { width, height } from '../../constants/layout';
 import { PropTypes } from './PropTypes';
 import config from '../../config';
 
-const PostProfile = ({ media }: PropTypes) => {
+const PostProfile = ({ post, openPosts }: PropTypes) => {
   return (
     <Box width={(width / 3) - 10} height={height / 6} borderWidth={2} borderColor={'white'}>
-      <Image style={{ width: '100%', height: '100%'}} source={{ uri: config.API + '/api/media/' + media}} />
+      <TouchableOpacity onPress={() => openPosts()}>
+        <Image style={{ width: '100%', height: '100%'}} source={{ uri: config.API + '/api/media/' + post.media}} />
+      </TouchableOpacity>
     </Box>
   )
 }
