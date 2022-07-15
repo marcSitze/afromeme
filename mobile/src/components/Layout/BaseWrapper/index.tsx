7ff90bc3b955c178cd5726d64ecbe7f242b5f3f8
marcSitze <marcsitze01@gmail.com>
2022-07-15 19:02:03 +0100

    format and pass new prop

--- mobile/src/components/Layout/BaseWrapper/index.tsx
+++ mobile/src/components/Layout/BaseWrapper/index.tsx
@@ -1,31 +1,46 @@
-import React from "react";
-import { Box, HStack, Text } from 'native-base';
-import { TouchableOpacity } from "react-native";
+import React from 'react';
+import {Box, HStack, Text} from 'native-base';
+import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
 type BaseWrapperProp = {
-  children: React.ReactNode,
-  headerText?: string,
-  backArrow?: Boolean,
-}
+  children: React.ReactNode;
+  headerText?: string;
+  backArrow?: Boolean;
+  backArrowAction: Function;
+};
 
-const BaseWrapper = ({ children, headerText = 'Afromeme', backArrow = false }: BaseWrapperProp) => {
+const BaseWrapper = ({
+  children,
+  headerText = 'Afromeme',
+  backArrow = false,
+  backArrowAction = () => {},
+}: BaseWrapperProp) => {
   const navigation: any = useNavigation();
   return (
     <Box bg="coolGray.100" height="full" px="2" pt={4}>
       {/**
        * @AppHeader @will @come @here
        */}
-       <HStack alignItems={'center'}>
-        {backArrow && <TouchableOpacity onPress={() => navigation.goBack()} style={{justifyContent: 'center', marginRight: 10}}>
-          <Icon name="arrow-back" size={30}/>
-        </TouchableOpacity>}
-        <Text fontWeight={'bold'} fontSize={'xl'}>{headerText}</Text>
-       </HStack>
+      <HStack alignItems={'center'}>
+        {backArrow && (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.goBack();
+              backArrowAction();
+            }}
+            style={{justifyContent: 'center', marginRight: 10}}>
+            <Icon name="arrow-back" size={30} />
+          </TouchableOpacity>
+        )}
+        <Text fontWeight={'bold'} fontSize={'xl'}>
+          {headerText}
+        </Text>
+      </HStack>
       {children}
     </Box>
-  )
-}
+  );
+};
 
 export default BaseWrapper;
