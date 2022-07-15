dc1a1e3a238571c884b8a388127b1f510690b668
marcSitze <marcsitze01@gmail.com>
2022-07-15 19:05:36 +0100

    view and clear profile actions

--- mobile/src/redux/users/actions.ts
+++ mobile/src/redux/users/actions.ts
@@ -9,3 +9,12 @@ export const getLocalUserAccount = (userId: string) => ({
   type: types.GET_LOCAL_USER_ACCOUNT_REQUEST,
   userId,
 });
+
+export const viewProfile = (accountId: string) => ({
+  type: types.VIEW_PROFILE_REQUEST,
+  payload: accountId,
+});
+
+export const clearViewProfile = () => ({
+  type: types.CLEAR_VIEW_PROFILE
+})
