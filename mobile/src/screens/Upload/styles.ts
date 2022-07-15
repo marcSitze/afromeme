a5bdf016a87e46d427c7496c3db6fa2153ca18b0
marcSitze <marcsitze01@gmail.com>
2022-07-16 00:13:12 +0100

    primary color

--- mobile/src/screens/Upload/styles.ts
+++ mobile/src/screens/Upload/styles.ts
@@ -1,12 +1,13 @@
 import { StyleSheet } from 'react-native';
 import { height } from '../../constants/layout';
+import colors from '../../constants/colors';
 
 const styles = StyleSheet.create({
   touchable: {
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
-    borderColor: 'blue',
+    borderColor: colors.light.primary,
     borderRadius: 6,
     width: '100%'
   }
