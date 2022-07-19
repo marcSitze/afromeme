648dd4c5ac9161af390ba283bfe5fcaf453720dd
marcSitze <marcsitze01@gmail.com>
2022-07-19 18:34:14 +0100

    todos repository

--- server/src/todos/todos.repository.ts
+++ server/src/todos/todos.repository.ts
@@ -15,12 +15,12 @@ export default function makeTodosRepository ({ database }: any) {
     console.log('database: ', database);
     console.log('database: ', db);
     try {
-      // const newTodo = await db.create(makeTodo(todo));
-      // const result = await newTodo.save(todo);
+      const newTodo = await db.create(makeTodo(todo));
+      const result = await newTodo.save(todo);
 
       return {
         success: true,
-        created: makeTodo(todo),
+        created: result,
       }
 
     }catch(err) {
