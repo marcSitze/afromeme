895fbd2345ae96a191582284355e431c8e3bbeb7
marcSitze <marcsitze01@gmail.com>
2022-07-19 18:32:40 +0100

    db distributor

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -3,24 +3,16 @@ import mongoose from 'mongoose';
 import Todo from '../models/Todo';
 
 export default async function makeDb (model: string) {
-  // const MongoClient = mongodb.MongoClient
-  // const url = 'mongodb://localhost:27017'
-  // const dbName = 'mm_api_demo'
-  // const client = new MongoClient(url, { useNewUrlParser: true })
-  // await client.connect()
   let db: any;
-  console.log('modelMakeDb: ', model)
-  // const db = mongoose.model(model, new mongoose.Schema({ message: String, done: Boolean }));
   switch(String(model)) {
-    case String('Todo'):
+    case 'Todo':
+      console.log('modelMakeDb: ', model)
       db = Todo;
+      break;
     default:
+      console.log('DefaultMakeDb: ', model)
       db = null;
   }
-  // const db = await client.db(dbName)
-  // db.makeId = makeIdFromString
+
   return db;
 }
-// function makeIdFromString (id) {
-//   return new mongodb.ObjectID(id)
-// }
