a6c7c3558eabadc3540d910b05c5b3a51baa25f3
marcSitze <marcsitze01@gmail.com>
2022-07-26 12:40:30 +0100

    import gravatar and dispatch action

--- mobile/src/components/Profile/index.tsx
+++ mobile/src/components/Profile/index.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import {Image, TouchableOpacity} from 'react-native';
 import {Box, Text} from 'native-base';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
+import {useDispatch} from 'react-redux';
 
 import ProfileImage from '../../assets/images/profile.png';
 import * as SCREENS from '../../constants/screens';
+import {IAccount} from '../../types/users';
+import {viewProfile} from '../../redux/users/actions';
+import Gravatar from '../Others/Gravatar'
 
-const Profile = () => {
+type PropTypes = {
+  account: IAccount;
+};
+
+const Profile = ({account}: PropTypes) => {
+  const dispatch = useDispatch();
   const navigation: any = useNavigation();
+  // console.log('Profiles: ', account);
+
+  const changeScreen = () => {
+    dispatch(viewProfile(account._id));
+    navigation.navigate(SCREENS.VIEW_PROFILE);
+  };
+
   return (
-    <TouchableOpacity onPress={() => navigation.navigate(SCREENS.VIEW_PROFILE)}>
-      <Box>
-        <Box bg={'blue.500'} borderRadius={40} mr={4}>
-          <Image style={{width: 80, height: 80}} source={ProfileImage} />
+    <TouchableOpacity onPress={changeScreen}>
+      <Box my={2} alignItems={'center'} mr={4}>
+        <Box borderRadius={40} mb={1}>
+          {/* <Image style={{width: 70, height: 70}} source={ProfileImage} /> */}
+          <Gravatar size={70} username={account.user?.username} />
         </Box>
-        <Text>John Doe</Text>
+        <Text fontSize={12} fontWeight={'semibold'}>
+          {account.user.username ?? 'John Doe'}
+        </Text>
       </Box>
     </TouchableOpacity>
   );
