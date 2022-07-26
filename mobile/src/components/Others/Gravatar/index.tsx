805915ea447d3c73af85a96628df31943a69aac8
marcSitze <marcsitze01@gmail.com>
2022-07-26 12:38:58 +0100

    Gravatar

---
+++ mobile/src/components/Others/Gravatar/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Gravatar from 'react-native-user-avatar';
+import styles from './styles';
+
+type PropsTypes = {
+  username: string;
+  size?: number;
+  style?: any;
+}
+const index = ({ username, size, style }: PropsTypes) => {
+  return (
+    <Gravatar style={[style ? style: {}]} size={size ?? 100} name={username} />
+  )
+}
+
+export default index
