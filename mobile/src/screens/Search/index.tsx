145c42a1893ebcdd292eb826cd48d50e1d1cef8b
marcSitze <marcsitze01@gmail.com>
2022-08-19 13:27:39 +0100

    search screen

--- mobile/src/screens/Search/index.tsx
+++ mobile/src/screens/Search/index.tsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react';
+import {Text, Input, Box, HStack} from 'native-base';
 
 const Search = () => {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+  const [text, setText] = useState('');
 
-export default Search
+  return (
+    <Box flex={1} px={3}>
+      <HStack alignItems={'center'}>
+        <Text mr={2} bg={'amber.400'}>back</Text>
+        <Input flex={1} value={text} onChangeText={text => setText(text)} />
+      </HStack>
+      <Text>Search</Text>
+    </Box>
+  );
+};
+
+export default Search;
