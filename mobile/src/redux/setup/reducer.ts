daaee3ee3a7a84bd0a751b3c38f9f1eed389019c
marcSitze <marcsitze01@gmail.com>
2022-08-20 11:48:42 +0100

    require search saga and reducer

--- mobile/src/redux/setup/reducer.ts
+++ mobile/src/redux/setup/reducer.ts
@@ -3,12 +3,14 @@ import postsReducer from "../posts/reducer";
 import authReducer from "../auth/reducer";
 import usersReducer from '../users/reducer'
 import commentsReducer from "../comments/reducer";
+import searchReducer from "../search/reducer";
 
 const reducerCombination = combineReducers({
     postsReducer,
     authReducer,
     usersReducer,
     commentsReducer,
+    searchReducer,
 });
 
 export default reducerCombination;
--- mobile/src/redux/setup/sagas.ts
+++ mobile/src/redux/setup/sagas.ts
@@ -1,13 +1,20 @@
-import { all } from 'redux-saga/effects';
+import {all} from 'redux-saga/effects';
 import PostsSaga from '../posts/saga';
 import AuthSaga from '../auth/saga';
 import UsersSaga from '../users/saga';
 import CommentsSaga from '../comments/saga';
+import SearchSaga from '../search/saga';
 
 /**
  * @description combine sagas
  */
 
 export default function* Sagas() {
-  yield all([PostsSaga(), AuthSaga(), UsersSaga(), CommentsSaga()]);
+  yield all([
+    PostsSaga(),
+    AuthSaga(),
+    UsersSaga(),
+    CommentsSaga(),
+    SearchSaga(),
+  ]);
 }
