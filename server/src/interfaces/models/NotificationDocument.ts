3734e97e493005a3e017738b0efca811e2a956d8
marcSitze <marcsitze01@gmail.com>
2022-08-24 20:48:50 +0100

    Notification document

--- server/src/interfaces/models/NotificationDocument.ts
+++ server/src/interfaces/models/NotificationDocument.ts
@@ -3,9 +3,10 @@ import { Document } from 'mongoose';
 export interface NotificationDocument extends Document{
     _id: string,
     type: string,
-    message: string,
+    message?: string,
     entityId: string,
     hasViewed: boolean,
+    author: string,
     createdAt?: Date,
     updatedAt?: Date,
     deletedAt?: Date
