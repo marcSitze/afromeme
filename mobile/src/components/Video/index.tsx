57bf5b6d56b2260ebd3646caf3127f1cd7a00e4c
marcSitze <marcsitze01@gmail.com>
2022-08-25 20:40:27 +0100

    video component

--- mobile/src/components/Video/index.tsx
+++ mobile/src/components/Video/index.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect} from 'react';
-import {Pressable} from 'react-native';
+import {TouchableOpacity} from 'react-native';
 import VideoComponent from 'react-native-video';
-import { Button, Text } from 'native-base'
+import {Box, Button, Text} from 'native-base';
+import colors from '../../constants/colors';
 
 type PropTypes = {
   link: string;
@@ -9,41 +10,40 @@ type PropTypes = {
 };
 const Video = ({link, isMuted}: PropTypes) => {
   const [isPlaying, setIsPlaying] = React.useState(false);
-  console.log('Link: ', link);
-
 
   return (
-    <>
-      {/* <Pressable onPress={() => {
-        console.log('Set pause')
-         setIsPlaying(!isPlaying)
-      }} style={{position: 'absolute', right: 10, bottom: 10 }}>
-        <Text bg={'pink.500'}>replay</Text>
-      </Pressable> */}
-      <Button onPress={() => {
-        console.log('Set pause')
-         setIsPlaying(!isPlaying)
-      }}
-      zIndex={10}
-      >replay</Button>
-      <VideoComponent
-        source={{uri: link}}
-        paused={!isPlaying}
-        // controls={true}
-        resizeMode="contain"
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          width: '100%',
-          height: '100%',
-          // backgroundColor: 'green',
-        }}
-        muted={isMuted}
-      />
-    </>
+    <TouchableOpacity
+      style={{flex: 1}}
+      onPress={() => {
+        console.log('Set pause');
+        setIsPlaying(!isPlaying);
+      }}>
+      {isPlaying ? (
+        <VideoComponent
+          source={{uri: link}}
+          paused={!isPlaying}
+          // controls={true}
+          resizeMode="contain"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            width: '100%',
+            height: '100%',
+            // backgroundColor: 'green',
+          }}
+          muted={isMuted}
+        />
+      ) : (
+        <Box flex={1} bg={colors.light.gray} justifyContent={'center'}>
+          <Text fontSize={'lg'} fontWeight={'bold'} textAlign={'center'}>
+            Tap To Play
+          </Text>
+        </Box>
+      )}
+    </TouchableOpacity>
   );
 };
 
